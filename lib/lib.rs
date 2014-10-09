bnoordhuis/v8.rs

commit 4059d0b3d812b6010b2487f3ffce4ab96c076b22

Call V8 API directly.
Remove the glue code and call V8 C++ API functions directly by mangled
name.

Probably needs to be revisited for MSVC because its name mangling scheme
produces symbols that are not valid Rust identifiers but I'll cross that
bridge when I come to it.

lib/lib.rs
@@ -1,447 +1,252 @@
 #![allow(non_snake_case)]
+
 extern crate libc;
 
 use std::default::Default;
 use std::ptr;
 
+#[link(name="v8")]
+extern {
+    fn _ZN2v87Context5EnterEv(this: *mut Context) -> ();
+    fn _ZN2v87Context4ExitEv(this: *mut Context) -> ();
+    fn _ZN2v87Context3NewEPNS_7IsolateEPNS_22ExtensionConfigurationENS_6HandleINS_14ObjectTemplateEEENS5_INS_5ValueEEE(
+            isolate: *mut Isolate, extensions: *mut ExtensionConfiguration,
+            global_template: *mut ObjectTemplate, global_object: *mut Value)
+            -> *mut Context;
+    fn _ZN2v811HandleScopeC1EPNS_7IsolateE(this: *mut HandleScope,
+                                           isolate: *mut Isolate) -> ();
+    fn _ZN2v811HandleScopeD1Ev(this: *mut HandleScope) -> ();
+    fn _ZN2v87Isolate3NewERKNS0_12CreateParamsE(
+            params: *const CreateParams) -> *mut Isolate;
+    fn _ZN2v87Isolate7DisposeEv(this: *mut Isolate) -> ();
+    fn _ZN2v87Isolate5EnterEv(this: *mut Isolate) -> ();
+    fn _ZN2v87Isolate4ExitEv(this: *mut Isolate) -> ();
+    fn _ZN2v86Script7CompileENS_6HandleINS_6StringEEEPNS_12ScriptOriginE(
+            source: *mut String, origin: *mut ScriptOrigin) -> *mut Script;
+    fn _ZN2v86Script3RunEv(this: *mut Script) -> *mut Value;
+    fn _ZN2v86String11NewFromUtf8EPNS_7IsolateEPKcNS0_13NewStringTypeEi(
+            isolate: *mut Isolate, data: *const u8, typ: NewStringType,
+            length: i32) -> *mut String;
+    fn _ZN2v82V810InitializeEv() -> bool;
+    fn _ZN2v82V87DisposeEv() -> bool;
+    fn _ZNK2v85Value8IsNumberEv(this: *mut Value) -> bool;
+}
+
 #[repr(C)]
-pub struct Local<T> {
-    val_: *const T,
+pub struct ExtensionConfiguration;
+
+#[repr(C)]
+pub struct ObjectTemplate;
+
+#[repr(C)]
+pub struct CreateParams {
+    entry_hook: *const u8,
+    code_event_handler: *const u8,
+    constraints: ResourceConstraints,
+    enable_serializer: bool,
 }
 
-#[repr(C)] pub struct Context;
-#[repr(C)] pub struct Isolate;
-#[repr(C)] pub struct Script;
-#[repr(C)] pub struct ScriptOrigin;
-#[repr(C)] pub struct String;
-#[repr(C)] pub struct TryCatch;
-#[repr(C)] pub struct Value;
-#[repr(C)] pub struct V8;
-
-pub fn with_context_scope(context: Local<Context>, closure: ||) {
-    (*context).Enter();
-    closure();
-    (*context).Exit();
-}
-
-pub fn with_isolate(closure: |&Isolate|) {
-    let isolate = Isolate::New();
-    closure(isolate);
-    drop(isolate);
-}
-
-pub fn with_isolate_scope(isolate: &Isolate, closure: ||) {
-    isolate.Enter();
-    closure();
-    isolate.Exit();
-}
-
-pub fn with_handle_scope(isolate: &Isolate, closure: ||) {
-    unsafe extern fn trampoline(closure: *const ||) {
-        (*closure)()
-    }
-    unsafe {
-        HandleScope_With(isolate, trampoline, &closure)
+impl Default for CreateParams {
+    fn default() -> CreateParams {
+        CreateParams {
+            entry_hook: 0 as *const u8,
+            code_event_handler: 0 as *const u8,
+            constraints: Default::default(),
+            enable_serializer: false,
+        }
     }
 }
 
-pub fn with_try_catch(closure: |&TryCatch|) {
-    unsafe extern fn trampoline(try_catch: &TryCatch,
-                                closure: *const |&TryCatch|) {
-        (*closure)(try_catch)
-    }
-    unsafe {
-        TryCatch_With(trampoline, &closure)
-    }
-}
-
-impl<T> Local<T> {
-    pub fn IsEmpty(&self) -> bool {
-        self.val_ == ptr::null()
-    }
-}
-
-impl<T> Default for Local<T> {
-    fn default() -> Local<T> {
-        Local { val_: ptr::null() }
-    }
-}
-
-impl<T> Deref<T> for Local<T> {
-    fn deref<'a>(&self) -> &T {
-        unsafe { &*self.val_ }
-    }
-}
+#[repr(C)]
+pub struct Context(*mut Context);
 
 impl Context {
     pub fn Enter(&self) {
-        unsafe { Context_Enter(self) }
+        match *self {
+            Context(this) => unsafe { _ZN2v87Context5EnterEv(this) }
+        }
     }
 
     pub fn Exit(&self) {
-        unsafe { Context_Exit(self) }
+        match *self {
+            Context(this) => unsafe { _ZN2v87Context4ExitEv(this) }
+        }
     }
 
-    pub fn New(isolate: &Isolate) -> Local<Context> {
-        unsafe { Context_New(isolate) }
+    pub fn New(isolate: &Isolate) -> Option<Context> {
+        maybe(Context, match *isolate {
+            Isolate(isolate) => unsafe {
+                _ZN2v87Context3NewEPNS_7IsolateEPNS_22ExtensionConfigurationENS_6HandleINS_14ObjectTemplateEEENS5_INS_5ValueEEE(
+                    isolate, ptr::null_mut(), ptr::null_mut(), ptr::null_mut())
+            }
+        })
     }
 }
 
-// FIXME(bnoordhuis) Should probably implement Drop and, er, drop Dispose().
-impl Isolate {
-    pub fn Dispose(&mut self) {
-        unsafe { Isolate_Dispose(self) }
-    }
+// Can't use a RAII type for Context::Scope because of
+// https://github.com/rust-lang/rust/issues/17858
+pub fn with_context_scope<T>(context: &Context, closure: || -> T) -> T {
+    context.Enter();
+    let rval = closure();
+    context.Exit();
+    rval
+}
 
+#[repr(C)]
+struct HandleScope([*mut u8, ..3]);
+
+pub fn with_handle_scope<T>(isolate: &Isolate, closure: || -> T) -> T {
+    let null = ptr::null_mut();
+    let mut this: HandleScope = HandleScope([null, null, null]);
+    match *isolate {
+        Isolate(isolate) => unsafe {
+            _ZN2v811HandleScopeC1EPNS_7IsolateE(&mut this, isolate)
+        }
+    };
+    let rval = closure();
+    unsafe { _ZN2v811HandleScopeD1Ev(&mut this) };
+    rval
+}
+
+#[repr(C)]
+pub struct Isolate(*mut Isolate);
+
+impl Isolate {
     pub fn Enter(&self) {
-        unsafe { Isolate_Enter(self) }
+        match *self {
+            Isolate(this) => unsafe { _ZN2v87Isolate5EnterEv(this) }
+        }
     }
 
     pub fn Exit(&self) {
-        unsafe { Isolate_Exit(self) }
+        match *self {
+            Isolate(this) => unsafe { _ZN2v87Isolate4ExitEv(this) }
+        }
     }
 
-    pub fn New<'a>() -> &'a mut Isolate {
-        unsafe { Isolate_New() }
+    pub fn New(_: Option<CreateParams>) -> Option<Isolate> {
+        let params = Default::default();
+        maybe(Isolate, unsafe {
+            _ZN2v87Isolate3NewERKNS0_12CreateParamsE(&params)
+        })
     }
 }
 
 impl Drop for Isolate {
     fn drop(&mut self) {
-        self.Dispose()
+        match *self {
+            Isolate(this) => unsafe { _ZN2v87Isolate7DisposeEv(this) }
+        }
     }
+}
+
+// Can't use a RAII type for Isolate::Scope because of
+// https://github.com/rust-lang/rust/issues/17858
+pub fn with_isolate_scope<T>(isolate: &Isolate, closure: || -> T) -> T {
+    isolate.Enter();
+    let rval = closure();
+    isolate.Exit();
+    rval
+}
+
+#[repr(C)]
+pub struct ResourceConstraints {
+    max_semi_space_size: i32,
+    max_old_space_size: i32,
+    max_executable_size: i32,
+    stack_limit: *mut u32,
+    max_available_threads: i32,
+    code_range_size: libc::size_t,
+}
+
+impl Default for ResourceConstraints {
+    fn default() -> ResourceConstraints {
+        ResourceConstraints {
+            max_semi_space_size: 0,
+            max_old_space_size: 0,
+            max_executable_size: 0,
+            stack_limit: ptr::null_mut(),
+            max_available_threads: 0,
+            code_range_size: 0,
+        }
+    }
+}
+
+#[repr(C)]
+pub struct Script(*mut Script);
+
+impl Script {
+    pub fn Compile(source: String,
+                   origin: Option<ScriptOrigin>) -> Option<Script> {
+        let _ = origin;
+        maybe(Script, match source {
+            String(source) => unsafe {
+                _ZN2v86Script7CompileENS_6HandleINS_6StringEEEPNS_12ScriptOriginE(
+                        source, ptr::null_mut())
+            }
+        })
+    }
+
+    pub fn Run(&self) -> Option<Value> {
+        maybe(Value, match *self {
+            Script(this) => unsafe { _ZN2v86Script3RunEv(this) }
+        })
+    }
+}
+
+#[repr(C)]
+pub struct ScriptOrigin;
+
+#[repr(C)]
+pub struct String(*mut String);
+
+#[repr(C)]
+#[deriving(Show)]
+pub enum NewStringType {
+    kNormalString,
+    kInternalizedString,
+    kUndetectableString,
 }
 
 impl String {
-    pub fn NewFromUtf8(isolate: &Isolate, data: *const u8, typ: i32,
-                       length: i32) -> Local<String> {
-        unsafe { String_NewFromUtf8(isolate, data, typ, length) }
+    pub fn NewFromUtf8(isolate: &Isolate, data: &str,
+                       typ: NewStringType) -> Option<String> {
+        maybe(String, match *isolate {
+            Isolate(isolate) => unsafe {
+                _ZN2v86String11NewFromUtf8EPNS_7IsolateEPKcNS0_13NewStringTypeEi(
+                        isolate, data.as_ptr(), typ, -1)
+            }
+        })
     }
 }
 
-fn to_local<T>(val: *const T) -> Local<T> {
-    Local { val_: val }
-}
-
-impl Script {
-    pub fn Compile(source: Local<String>,
-                   _: Option<ScriptOrigin>) -> Local<Script> {
-        unsafe { Script_Compile(source, ptr::null()) }
-    }
-
-    pub fn Run(&self) -> Local<Value> {
-        unsafe { Script_Run(to_local(self)) }
-    }
-}
-
-impl ScriptOrigin {
-}
-
-impl TryCatch {
-    pub fn CanContinue(&self) -> bool {
-        unsafe { TryCatch_CanContinue(self) }
-    }
-
-    pub fn Exception(&self) -> Local<Value> {
-        unsafe { TryCatch_Exception(self) }
-    }
-
-    pub fn HasCaught(&self) -> bool {
-        unsafe { TryCatch_HasCaught(self) }
-    }
-
-    pub fn HasTerminated(&self) -> bool {
-        unsafe { TryCatch_HasTerminated(self) }
-    }
-
-    pub fn ReThrow(&self) -> Local<Value> {
-        unsafe { TryCatch_ReThrow(self) }
-    }
-
-    pub fn Reset(&self) {
-        unsafe { TryCatch_Reset(self) }
-    }
-
-    pub fn StackTrace(&self) -> Local<Value> {
-        unsafe { TryCatch_StackTrace(self) }
-    }
-}
+#[repr(C)]
+pub struct V8(*mut V8);
 
 impl V8 {
-    pub fn Dispose() -> bool {
-        return unsafe { V8_Dispose() }
+    pub fn Initialize() -> bool {
+        unsafe { _ZN2v82V810InitializeEv() }
     }
 
-    pub fn Initialize() -> bool {
-        return unsafe { V8_Initialize() }
+    pub fn Dispose() -> bool {
+        unsafe { _ZN2v82V87DisposeEv() }
     }
 }
+
+#[repr(C)]
+pub struct Value(*mut Value);
 
 impl Value {
-    pub fn BooleanValue(&self) -> bool {
-        unsafe { Value_BooleanValue(self) }
-    }
-
-    pub fn Int32Value(&self) -> i32 {
-        unsafe { Value_Int32Value(self) }
-    }
-
-    pub fn IntegerValue(&self) -> i64 {
-        unsafe { Value_IntegerValue(self) }
-    }
-
-    pub fn IsArgumentsObject(&self) -> bool {
-        unsafe { Value_IsArgumentsObject(self) }
-    }
-
-    pub fn IsArray(&self) -> bool {
-        unsafe { Value_IsArray(self) }
-    }
-
-    pub fn IsArrayBuffer(&self) -> bool {
-        unsafe { Value_IsArrayBuffer(self) }
-    }
-
-    pub fn IsArrayBufferView(&self) -> bool {
-        unsafe { Value_IsArrayBufferView(self) }
-    }
-
-    pub fn IsBoolean(&self) -> bool {
-        unsafe { Value_IsBoolean(self) }
-    }
-
-    pub fn IsBooleanObject(&self) -> bool {
-        unsafe { Value_IsBooleanObject(self) }
-    }
-
-    pub fn IsDataView(&self) -> bool {
-        unsafe { Value_IsDataView(self) }
-    }
-
-    pub fn IsDate(&self) -> bool {
-        unsafe { Value_IsDate(self) }
-    }
-
-    pub fn IsExternal(&self) -> bool {
-        unsafe { Value_IsExternal(self) }
-    }
-
-    pub fn IsFalse(&self) -> bool {
-        unsafe { Value_IsFalse(self) }
-    }
-
-    pub fn IsFloat32Array(&self) -> bool {
-        unsafe { Value_IsFloat32Array(self) }
-    }
-
-    pub fn IsFloat64Array(&self) -> bool {
-        unsafe { Value_IsFloat64Array(self) }
-    }
-
-    pub fn IsFunction(&self) -> bool {
-        unsafe { Value_IsFunction(self) }
-    }
-
-    pub fn IsGeneratorFunction(&self) -> bool {
-        unsafe { Value_IsGeneratorFunction(self) }
-    }
-
-    pub fn IsGeneratorObject(&self) -> bool {
-        unsafe { Value_IsGeneratorObject(self) }
-    }
-
-    pub fn IsInt16Array(&self) -> bool {
-        unsafe { Value_IsInt16Array(self) }
-    }
-
-    pub fn IsInt32(&self) -> bool {
-        unsafe { Value_IsInt32(self) }
-    }
-
-    pub fn IsInt32Array(&self) -> bool {
-        unsafe { Value_IsInt32Array(self) }
-    }
-
-    pub fn IsInt8Array(&self) -> bool {
-        unsafe { Value_IsInt8Array(self) }
-    }
-
-    pub fn IsMap(&self) -> bool {
-        unsafe { Value_IsMap(self) }
-    }
-
-    pub fn IsName(&self) -> bool {
-        unsafe { Value_IsName(self) }
-    }
-
-    pub fn IsNativeError(&self) -> bool {
-        unsafe { Value_IsNativeError(self) }
-    }
-
-    pub fn IsNull(&self) -> bool {
-        unsafe { Value_IsNull(self) }
-    }
-
     pub fn IsNumber(&self) -> bool {
-        unsafe { Value_IsNumber(self) }
-    }
-
-    pub fn IsNumberObject(&self) -> bool {
-        unsafe { Value_IsNumberObject(self) }
-    }
-
-    pub fn IsObject(&self) -> bool {
-        unsafe { Value_IsObject(self) }
-    }
-
-    pub fn IsPromise(&self) -> bool {
-        unsafe { Value_IsPromise(self) }
-    }
-
-    pub fn IsRegExp(&self) -> bool {
-        unsafe { Value_IsRegExp(self) }
-    }
-
-    pub fn IsSet(&self) -> bool {
-        unsafe { Value_IsSet(self) }
-    }
-
-    pub fn IsString(&self) -> bool {
-        unsafe { Value_IsString(self) }
-    }
-
-    pub fn IsStringObject(&self) -> bool {
-        unsafe { Value_IsStringObject(self) }
-    }
-
-    pub fn IsSymbol(&self) -> bool {
-        unsafe { Value_IsSymbol(self) }
-    }
-
-    pub fn IsSymbolObject(&self) -> bool {
-        unsafe { Value_IsSymbolObject(self) }
-    }
-
-    pub fn IsTrue(&self) -> bool {
-        unsafe { Value_IsTrue(self) }
-    }
-
-    pub fn IsTypedArray(&self) -> bool {
-        unsafe { Value_IsTypedArray(self) }
-    }
-
-    pub fn IsUint16Array(&self) -> bool {
-        unsafe { Value_IsUint16Array(self) }
-    }
-
-    pub fn IsUint32(&self) -> bool {
-        unsafe { Value_IsUint32(self) }
-    }
-
-    pub fn IsUint32Array(&self) -> bool {
-        unsafe { Value_IsUint32Array(self) }
-    }
-
-    pub fn IsUint8Array(&self) -> bool {
-        unsafe { Value_IsUint8Array(self) }
-    }
-
-    pub fn IsUint8ClampedArray(&self) -> bool {
-        unsafe { Value_IsUint8ClampedArray(self) }
-    }
-
-    pub fn IsUndefined(&self) -> bool {
-        unsafe { Value_IsUndefined(self) }
-    }
-
-    pub fn IsWeakMap(&self) -> bool {
-        unsafe { Value_IsWeakMap(self) }
-    }
-
-    pub fn IsWeakSet(&self) -> bool {
-        unsafe { Value_IsWeakSet(self) }
-    }
-
-    pub fn NumberValue(&self) -> f64 {
-        unsafe { Value_NumberValue(self) }
+        match *self {
+            Value(this) => unsafe { _ZNK2v85Value8IsNumberEv(this) }
+        }
     }
 }
 
-#[link(name="glue")]
-#[link(name="v8")]
-#[link(name="stdc++")]  // __gxx_personality_v0@@CXXABI_1.3
-extern {
-    fn HandleScope_With(isolate: &Isolate,
-                        callback: unsafe extern fn(*const ||),
-                        closure: *const ||);
-    fn Context_Enter(context: &Context);
-    fn Context_Exit(context: &Context);
-    fn Context_New(isolate: &Isolate) -> Local<Context>;
-    fn Isolate_Dispose(isolate: &mut Isolate);
-    fn Isolate_Enter(isolate: &Isolate);
-    fn Isolate_Exit(isolate: &Isolate);
-    fn Isolate_New() -> &mut Isolate;
-    fn Script_Compile(source: Local<String>,
-                      origin: *const ScriptOrigin) -> Local<Script>;
-    fn Script_Run(script: Local<Script>) -> Local<Value>;
-    fn String_NewFromUtf8(isolate: &Isolate, data: *const u8, typ: i32,
-                          length: i32) -> Local<String>;
-    fn TryCatch_CanContinue(try_catch: &TryCatch) -> bool;
-    fn TryCatch_Exception(try_catch: &TryCatch) -> Local<Value>;
-    fn TryCatch_HasCaught(try_catch: &TryCatch) -> bool;
-    fn TryCatch_HasTerminated(try_catch: &TryCatch) -> bool;
-    fn TryCatch_ReThrow(try_catch: &TryCatch) -> Local<Value>;
-    fn TryCatch_Reset(try_catch: &TryCatch);
-    fn TryCatch_StackTrace(try_catch: &TryCatch) -> Local<Value>;
-    fn TryCatch_With(callback: unsafe extern fn(&TryCatch, *const |&TryCatch|),
-                     closure: *const |&TryCatch|);
-    fn V8_Dispose() -> bool;
-    fn V8_Initialize() -> bool;
-    fn Value_BooleanValue(that: &Value) -> bool;
-    fn Value_Int32Value(that: &Value) -> i32;
-    fn Value_IntegerValue(that: &Value) -> i64;
-    fn Value_IsArgumentsObject(that: &Value) -> bool;
-    fn Value_IsArray(that: &Value) -> bool;
-    fn Value_IsArrayBuffer(that: &Value) -> bool;
-    fn Value_IsArrayBufferView(that: &Value) -> bool;
-    fn Value_IsBoolean(that: &Value) -> bool;
-    fn Value_IsBooleanObject(that: &Value) -> bool;
-    fn Value_IsDataView(that: &Value) -> bool;
-    fn Value_IsDate(that: &Value) -> bool;
-    fn Value_IsExternal(that: &Value) -> bool;
-    fn Value_IsFalse(that: &Value) -> bool;
-    fn Value_IsFloat32Array(that: &Value) -> bool;
-    fn Value_IsFloat64Array(that: &Value) -> bool;
-    fn Value_IsFunction(that: &Value) -> bool;
-    fn Value_IsGeneratorFunction(that: &Value) -> bool;
-    fn Value_IsGeneratorObject(that: &Value) -> bool;
-    fn Value_IsInt16Array(that: &Value) -> bool;
-    fn Value_IsInt32(that: &Value) -> bool;
-    fn Value_IsInt32Array(that: &Value) -> bool;
-    fn Value_IsInt8Array(that: &Value) -> bool;
-    fn Value_IsMap(that: &Value) -> bool;
-    fn Value_IsName(that: &Value) -> bool;
-    fn Value_IsNativeError(that: &Value) -> bool;
-    fn Value_IsNull(that: &Value) -> bool;
-    fn Value_IsNumber(that: &Value) -> bool;
-    fn Value_IsNumberObject(that: &Value) -> bool;
-    fn Value_IsObject(that: &Value) -> bool;
-    fn Value_IsPromise(that: &Value) -> bool;
-    fn Value_IsRegExp(that: &Value) -> bool;
-    fn Value_IsSet(that: &Value) -> bool;
-    fn Value_IsString(that: &Value) -> bool;
-    fn Value_IsStringObject(that: &Value) -> bool;
-    fn Value_IsSymbol(that: &Value) -> bool;
-    fn Value_IsSymbolObject(that: &Value) -> bool;
-    fn Value_IsTrue(that: &Value) -> bool;
-    fn Value_IsTypedArray(that: &Value) -> bool;
-    fn Value_IsUint16Array(that: &Value) -> bool;
-    fn Value_IsUint32(that: &Value) -> bool;
-    fn Value_IsUint32Array(that: &Value) -> bool;
-    fn Value_IsUint8Array(that: &Value) -> bool;
-    fn Value_IsUint8ClampedArray(that: &Value) -> bool;
-    fn Value_IsUndefined(that: &Value) -> bool;
-    fn Value_IsWeakMap(that: &Value) -> bool;
-    fn Value_IsWeakSet(that: &Value) -> bool;
-    fn Value_NumberValue(that: &Value) -> f64;
+fn maybe<T>(make: |*mut T| -> T, val: *mut T) -> Option<T> {
+    match val as uint {
+        0 => None,
+        _ => Some(make(val)),
+    }
 }

lib/glue/api.cc
@@ -1,141 +1,0 @@
-#pragma GCC diagnostic ignored "-Wunused-parameter"
-#include "v8.h"
-#pragma GCC diagnostic warning "-Wunused-parameter"
-
-namespace {
-
-extern "C" void Context_Enter(v8::Local<v8::Context> context) {
-  return context->Enter();
-}
-
-extern "C" void Context_Exit(v8::Local<v8::Context> context) {
-  return context->Exit();
-}
-
-extern "C" v8::Local<v8::Context> Context_New(v8::Isolate* isolate) {
-  return v8::Context::New(isolate);
-}
-
-extern "C" void HandleScope_With(v8::Isolate* isolate, void (*callback)(void*),
-                                 void* arg) {
-  v8::HandleScope handle_scope(isolate);
-  callback(arg);
-}
-
-extern "C" void Isolate_Dispose(v8::Isolate* isolate) {
-  return isolate->Dispose();
-}
-
-extern "C" void Isolate_Enter(v8::Isolate* isolate) {
-  return isolate->Enter();
-}
-
-extern "C" void Isolate_Exit(v8::Isolate* isolate) {
-  return isolate->Exit();
-}
-
-extern "C" v8::Isolate* Isolate_New() {
-  return v8::Isolate::New();
-}
-
-extern "C" bool V8_Dispose() {
-  return v8::V8::Dispose();
-}
-
-extern "C" bool V8_Initialize() {
-  return v8::V8::Initialize();
-}
-
-extern "C" v8::Local<v8::Script> Script_Compile(v8::Local<v8::String> source,
-                                                v8::ScriptOrigin* origin) {
-  return v8::Script::Compile(source, origin);
-}
-
-extern "C" v8::Local<v8::Value> Script_Run(v8::Local<v8::Script> script) {
-  return script->Run();
-}
-
-extern "C" v8::Local<v8::String> String_NewFromUtf8(
-    v8::Isolate* isolate,
-    const char* data,
-    v8::String::NewStringType type,
-    int length) {
-  return v8::String::NewFromUtf8(isolate, data, type, length);
-}
-
-extern "C" void TryCatch_With(void (*callback)(v8::TryCatch*, void*),
-                              void* arg) {
-  v8::TryCatch try_catch;
-  callback(&try_catch, arg);
-}
-
-#define V(clazz, method, type) \
-  extern "C" type clazz##_##method(v8::clazz* that) { return that->method(); }
-V(TryCatch, CanContinue, bool)
-V(TryCatch, Exception, v8::Local<v8::Value>)
-V(TryCatch, HasCaught, bool)
-V(TryCatch, HasTerminated, bool)
-V(TryCatch, Message, v8::Local<v8::Message>)
-V(TryCatch, ReThrow, v8::Local<v8::Value>)
-V(TryCatch, Reset, void)
-V(TryCatch, StackTrace, v8::Local<v8::Value>)
-V(Value, BooleanValue, bool);
-V(Value, Int32Value, int32_t);
-V(Value, IntegerValue, int64_t);
-V(Value, IsArgumentsObject, bool);
-V(Value, IsArray, bool);
-V(Value, IsArrayBuffer, bool);
-V(Value, IsArrayBufferView, bool);
-V(Value, IsBoolean, bool);
-V(Value, IsBooleanObject, bool);
-V(Value, IsDataView, bool);
-V(Value, IsDate, bool);
-V(Value, IsExternal, bool);
-V(Value, IsFalse, bool);
-V(Value, IsFloat32Array, bool);
-V(Value, IsFloat64Array, bool);
-V(Value, IsFunction, bool);
-V(Value, IsGeneratorFunction, bool);
-V(Value, IsGeneratorObject, bool);
-V(Value, IsInt16Array, bool);
-V(Value, IsInt32, bool);
-V(Value, IsInt32Array, bool);
-V(Value, IsInt8Array, bool);
-V(Value, IsMap, bool);
-V(Value, IsName, bool);
-V(Value, IsNativeError, bool);
-V(Value, IsNull, bool);
-V(Value, IsNumber, bool);
-V(Value, IsNumberObject, bool);
-V(Value, IsObject, bool);
-V(Value, IsPromise, bool);
-V(Value, IsRegExp, bool);
-V(Value, IsSet, bool);
-V(Value, IsString, bool);
-V(Value, IsStringObject, bool);
-V(Value, IsSymbol, bool);
-V(Value, IsSymbolObject, bool);
-V(Value, IsTrue, bool);
-V(Value, IsTypedArray, bool);
-V(Value, IsUint16Array, bool);
-V(Value, IsUint32, bool);
-V(Value, IsUint32Array, bool);
-V(Value, IsUint8Array, bool);
-V(Value, IsUint8ClampedArray, bool);
-V(Value, IsUndefined, bool);
-V(Value, IsWeakMap, bool);
-V(Value, IsWeakSet, bool);
-V(Value, NumberValue, double);
-V(Value, ToArrayIndex, v8::Local<v8::Uint32>);
-V(Value, ToBoolean, v8::Local<v8::Boolean>);
-V(Value, ToDetailString, v8::Local<v8::String>);
-V(Value, ToInt32, v8::Local<v8::Int32>);
-V(Value, ToInteger, v8::Local<v8::Integer>);
-V(Value, ToNumber, v8::Local<v8::Number>);
-V(Value, ToObject, v8::Local<v8::Object>);
-V(Value, ToString, v8::Local<v8::String>);
-V(Value, ToUint32, v8::Local<v8::Uint32>);
-V(Value, Uint32Value, uint32_t);
-#undef V
-
-}  // namespace anonymous

lib/glue/makefile.cargo
@@ -1,8 +1,0 @@
-CXXFLAGS = -Wall -Wextra -I../../include -fno-exceptions -fno-rtti -fPIC \
-           -fvisibility=hidden
-
-$(OUT_DIR)/libglue.a: $(OUT_DIR)/api.o
-	$(AR) crs $@ $^
-
-$(OUT_DIR)/api.o: api.cc
-	$(CXX) $(CXXFLAGS) $< -c -o $@

makefile.cargo
@@ -20,7 +20,8 @@ endif
 V8_TARGET := $(V8_ARCH).$(V8_MODE)
 
 .PHONY: all
-all:
-	$(MAKE) -f makefile.cargo -C lib/glue/
+all: $(OUT_DIR)/libv8.so
+
+$(OUT_DIR)/libv8.so:
 	$(MAKE) -f makefile.v8 -j $(NUM_JOBS) $(V8_TARGET) $(V8_FLAGS)
-	ln -fs $(OUT_DIR)/$(V8_TARGET)/lib.target/libv8.so $(OUT_DIR)/libv8.so
+	ln -fs $(OUT_DIR)/$(V8_TARGET)/lib.target/libv8.so $@

tests/basic.rs
@@ -1,28 +1,23 @@
 extern crate v8;
 
 #[test]
-fn smoketest() {
+fn smoketest0() {
     assert!(v8::V8::Initialize());
-    v8::with_isolate(|isolate| {
-        v8::with_isolate_scope(isolate, || {
-            v8::with_handle_scope(isolate, || {
-                let context = v8::Context::New(isolate);
-                v8::with_context_scope(context, || {
-                    let raw_source = "42*42".as_ptr();
+    {
+        let isolate = v8::Isolate::New(None).unwrap();
+        v8::with_handle_scope(&isolate, || {
+            v8::with_isolate_scope(&isolate, || {
+                let context = v8::Context::New(&isolate).unwrap();
+                v8::with_context_scope(&context, || {
                     let source =
-                            v8::String::NewFromUtf8(isolate, raw_source, 0, -1);
-                    v8::with_try_catch(|try_catch| {
-                        let script = v8::Script::Compile(source, None);
-                        assert!(!try_catch.HasCaught());
-                        let result = script.Run();
-                        assert!(!try_catch.HasCaught());
-                        assert!(!result.IsEmpty());
-                        assert!((*result).IsNumber());
-                        assert!(1764. == (*result).NumberValue());
-                    });
+                            v8::String::NewFromUtf8(&isolate, "42",
+                                                    v8::kNormalString).unwrap();
+                    let script = v8::Script::Compile(source, None).unwrap();
+                    let result = script.Run().unwrap();
+                    assert!(result.IsNumber());
                 });
             });
         });
-    });
+    }
     assert!(v8::V8::Dispose());
 }

commit 785e44a9f3cce4371685f6bcd32f09c2fdfcc3d1

Fix length argument to v8::String::NewFromUtf8().
Rust strings are not nul-terminated, v8.rs needs to explicitly pass the
string length to V8.

Bug introduced in commit 4059d0b ("Call V8 API directly.") and at least
partially responsible for the failing native_api_call test.

lib/lib.rs
@@ -619,7 +619,7 @@ impl String {
                        typ: NewStringType) -> Option<String> {
         unsafe {
             _ZN2v86String11NewFromUtf8EPNS_7IsolateEPKcNS0_13NewStringTypeEi(
-                    isolate, data.as_ptr(), typ, -1)
+                    isolate, data.as_ptr(), typ, data.len() as i32)
         }.to_option()
     }
 }